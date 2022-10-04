0970e12fbec7148d8983b02e978639e0b4822921
ilhwan <poj0722@naver.com>
2022-10-04 23:36:04 +0900

    :pencil2: 옵저버 패턴 psvm 오타 수정

--- src/main/java/com/pppp0722/designpattern/observer/Main.java
+++ src/main/java/com/pppp0722/designpattern/observer/Main.java
@@ -5,9 +5,9 @@ public class Main {
     public static void main(String[] args) {
         WeatherData weatherData = new WeatherData();
 
-        DailyDisplay currentDisplay = new DailyDisplay(weatherData);
+        DailyDisplay dailyDisplay = new DailyDisplay(weatherData);
         weatherData.setMeasurements(80.0d, 65.0d, 1020.8d);
-        weatherData.removeObserver(currentDisplay);
+        weatherData.removeObserver(dailyDisplay);
         weatherData.setMeasurements(79.0d, 64.0d, 1019.8d);
 
         DetailDisplay detailDisplay = new DetailDisplay(weatherData);
